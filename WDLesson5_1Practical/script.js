c535e7955d567e889aca1da6b8e7d7f5fa7f7321
moazzamc <moazzamc@nycstudents.net>
2026-03-06 18:49:00 +0000

    aaaaaaaaaa

--- WDLesson5_1Practical/script.js
+++ WDLesson5_1Practical/script.js
@@ -13,17 +13,20 @@ Year 3: $xxxxxx
 ...
 Year N: $xxxxxx
 */ 
-function calcinterest() {
+function calcinterest(){
     let p = parseFloat(document.getElementById("p").value);
     let r = parseFloat(document.getElementById("r").value);
-    let i = parseFloat(document.getElementById("n").value);
+    let n = parseFloat(document.getElementById("n").value);
     let yrs = parseInt (document.getElementById("t").value);
 
-    let op = (document.getElementById("op");
-    let build = ;
+    let op = document.getElementById("output");
+    let build = "";
 
-    for(let t = 0, t< yrs, t++){
-    
+    for(let t = 0; t<= yrs; t+= 1){
+        let A = (Math.pow(1 + r / n, n * t))*p;
+
+    build += `Year ${t}: $ ${A} <br>`;
+
     }
     op.innerHTML = build;
 
